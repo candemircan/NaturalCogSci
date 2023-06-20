cd72dc4f2e2bfb7b021e079eb77ee1a69bf8b409
Can Demircan <cdemircan@tutanota.com>
2023-06-20 15:45:02 +0200

    fix paths to *sh in docs

--- nbs/bin/cka.sh
+++ nbs/bin/cka.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-for target in task clip_RN50 clip_RN101 clip_RN50x4 clip_ViT-B_16 clip_ViT-B_32 clip_ViT-L_14;
-do 
-    sbatch "$NATURALCOGSCI_ROOT"/src/sh/cka.slurm \
+for target in task clip_RN50 clip_RN101 clip_RN50x4 clip_ViT-B_16 clip_ViT-B_32 clip_ViT-L_14; do
+    sbatch "$NATURALCOGSCI_ROOT"/nbs/bin/cka.slurm \
         -f all -t $target
 done
--- nbs/bin/loo_cv.sh
+++ nbs/bin/loo_cv.sh
@@ -2,13 +2,13 @@
 
 embeddings=$(jq -c -r 'keys_unsorted[]' "$NATURALCOGSCI_ROOT"/data/model_configs.json)
 
-for experiment in reward_learning category_learning;do
+for experiment in reward_learning category_learning; do
     for features in $embeddings; do
         for transform in original pca; do
-            for regularisation in l1 l2;do
+            for regularisation in l1 l2; do
 
-    sbatch "$NATURALCOGSCI_ROOT"/src/sh/loo_cv.slurm \
-        -e $experiment -f "$features" -t $transform -r $regularisation
+                sbatch "$NATURALCOGSCI_ROOT"/nbs/bin/loo_cv.slurm \
+                    -e $experiment -f "$features" -t $transform -r $regularisation
             done
         done
     done
